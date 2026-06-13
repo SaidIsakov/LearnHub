ef5edb3c5587a5ffe8444800dde74c9416eee9cf
Said <pm5581287@gmail.com>
2026-06-14 00:49:21 +0300

    Add Telegram notification for new student

--- backend/apps/courses/tasks.py
+++ backend/apps/courses/tasks.py
@@ -1,6 +1,12 @@
 from celery import shared_task
 from apps.courses.models import Course, CourseRole, CourseMember
 from apps.users.models import User
+from apps.courses.telegram import send_message
+from decouple import config
+
+
+BOT_TOKEN = config('TELEGRAM_BOT_TOKEN')
+CHAT_ID = config('TELEGRAM_CHAT_ID')
 
 
 @shared_task
@@ -9,4 +15,10 @@ def notify_new_student(course_id, student_id):
   student = User.objects.get(id=student_id)
   instructor = CourseMember.objects.get(role=CourseRole.INSTRUCTOR, course=course)
 
-  return f'На курс {course.title} записался {student.username} - инструктор {instructor.user.username}'
+  message = f'На курс {course.title} записался {student.username} - инструктор {instructor.user.username}'
+
+  send_message(
+    chat_id=CHAT_ID,
+    text=message
+  )
+
---
+++ backend/apps/courses/telegram.py
@@ -0,0 +1,18 @@
+import telegram
+import asyncio
+from decouple import config
+
+
+BOT_TOKEN = config('TELEGRAM_BOT_TOKEN')
+CHAT_ID = config('TELEGRAM_CHAT_ID')
+
+
+async def _send_message(chat_id: str, text: str) -> None:
+  """Отправляет сообщение в Telegram"""
+  bot = telegram.Bot(token=config('TELEGRAM_BOT_TOKEN'))
+  await bot.send_message(chat_id=chat_id, text=text)
+
+
+def send_message(chat_id: str, text: str) -> None:
+  """Синхронная обёртка для отправки сообщения"""
+  asyncio.run(_send_message(chat_id, text))
