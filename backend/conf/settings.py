59fc7a0333dd37c4ea979cadafee86ddf2c0b2cb
Said <pm5581287@gmail.com>
2026-06-21 13:17:23 +0300

    Full Docker containerization (Django + Celery in containers)

--- backend/conf/settings.py
+++ backend/conf/settings.py
@@ -179,8 +179,8 @@ SPECTACULAR_SETTINGS = {
 
 
 # Celery настройки
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = 'redis://redis:6379/0'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 
