55367d14e65fb317874baefde545a266c9a23adb
Said <pm5581287@gmail.com>
2026-06-29 01:15:58 +0300

    Add tests for AI chat assistant with monkeypatch

---
+++ backend/apps/courses/tests/test_ai.py
@@ -0,0 +1,101 @@
+import pytest
+from apps.courses.models import Lesson, CourseMember, CourseRole
+from apps.courses import ai_assistant
+from rest_framework import status
+
+
+@pytest.mark.django_db
+def test_ai_answer_quetion(create_user, create_api_client,  monkeypatch, course):
+  """
+    Успешный вопрос к AI
+  """
+  monkeypatch.setattr('apps.courses.views.ask_ai_about_lesson', lambda lesson_content, question : 'answer')
+
+  alex = create_user('Alex')
+  misha = create_user('Misha')
+  lesson = Lesson.objects.create(
+      title='Test_lesson',
+      content='Test_content',
+      course=course,
+      created_by = alex
+  )
+  instructor = CourseMember.objects.create(
+      course=course,
+      user=alex,
+      role=CourseRole.INSTRUCTOR
+  )
+  student = CourseMember.objects.create(
+    course=course,
+    user=misha,
+    role=CourseRole.STUDENT
+  )
+
+  url = f'/api/lessons/{lesson.id}/ask/'
+  client = create_api_client(misha)
+  response = client.post(url, {"question": "Объясни тему"})
+
+  assert response.data.get('answer') is not None
+
+
+@pytest.mark.django_db
+def test_quetion_without_text(create_user, create_api_client,  monkeypatch, course):
+  """
+    Вопрос без текста (валидация)
+  """
+
+  monkeypatch.setattr('apps.courses.views.ask_ai_about_lesson', lambda lesson_content, question : 'answer')
+
+  alex = create_user('Alex')
+  misha = create_user('Misha')
+  lesson = Lesson.objects.create(
+      title='Test_lesson',
+      content='Test_content',
+      course=course,
+      created_by = alex
+  )
+  instructor = CourseMember.objects.create(
+      course=course,
+      user=alex,
+      role=CourseRole.INSTRUCTOR
+  )
+  student = CourseMember.objects.create(
+    course=course,
+    user=misha,
+    role=CourseRole.STUDENT
+  )
+
+  url = f'/api/lessons/{lesson.id}/ask/'
+  client = create_api_client(misha)
+  response = client.post(url, {"question": ""})
+
+  assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+
+
+@pytest.mark.django_db
+def test_user_not_course_member(create_user, create_api_client,  monkeypatch, course):
+  """
+    Пользователь не участник курса
+  """
+
+  monkeypatch.setattr('apps.courses.views.ask_ai_about_lesson', lambda lesson_content, question : 'answer')
+
+  alex = create_user('Alex')
+  misha = create_user('Misha')
+  lesson = Lesson.objects.create(
+      title='Test_lesson',
+      content='Test_content',
+      course=course,
+      created_by = alex
+  )
+  instructor = CourseMember.objects.create(
+      course=course,
+      user=alex,
+      role=CourseRole.INSTRUCTOR
+  )
+
+  url = f'/api/lessons/{lesson.id}/ask/'
+  client = create_api_client(misha)
+  response = client.post(url, {"question": "Test question"})
+
+  assert response.status_code == status.HTTP_404_NOT_FOUND
